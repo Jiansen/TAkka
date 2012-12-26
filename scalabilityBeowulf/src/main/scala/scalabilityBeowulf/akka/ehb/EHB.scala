4680fe47e702460263b48452eb11d4a084bcf86d
Jiansen <jiansenhe@gmail.com>
2012-12-26 18:28:01 +0000

    GenStress init

---
+++ scalabilityBeowulf/src/main/scala/scalabilityBeowulf/akka/ehb/EHB.scala
@@ -0,0 +1,187 @@
+/*
+This is an implementation of hackbench in Erlang, a benchmark 
+and stress test for Linux schedulers. The number of groups and 
+the number of messages that each sender should send to each 
+receiver in the same group are the two parameters that this 
+benchmark receives.
+ */
+package scalabilityBeowulf.akka.ehb
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import util.{BenchTimer, BenchCounter}
+import akka.remote._
+import com.typesafe.config.ConfigFactory
+import scalabilityBeowulf.BeowulfConfig._
+
+sealed trait GroupMsg
+sealed trait MasterMsg
+sealed trait ReceiverMsg
+sealed trait SenderMsg
+
+case object SenderDone extends ReceiverMsg
+case class ReceiverDone(r:ActorRef) extends GroupMsg
+case class RUKeepingUp(from:ActorRef) extends ReceiverMsg
+case class IamKeeypingUp(receiver:ActorRef) extends SenderMsg
+
+case class DummyDATA(data:List[String]) extends ReceiverMsg
+
+object EHBConstant {
+  val ACK = 20
+  val DATA = DummyDATA(List("a", "b", "c", "d", "e", "f", "g",
+                  "h", "i", "j", "k", "l") )
+  val GSIZE = 20
+}
+
+
+case class MasterGO(groups:Int, loops:Int) extends MasterMsg
+case object GroupGO extends GroupMsg
+case class GroupInit(master:ActorRef, loops:Int) extends GroupMsg
+case class GroupReady(g:ActorRef) extends MasterMsg
+case class GroupDone(g:ActorRef) extends MasterMsg
+
+class MasterActor extends Actor{
+  val master = self
+  val timer = new BenchTimer
+  var gs:List[ActorRef] = _
+  val readyCounter = new BenchCounter
+  val doneCounter = new BenchCounter
+  def receive = {
+    case MasterGO(groups, loops) =>
+      readyCounter.set(groups)
+      doneCounter.set(groups)
+      gs = (for (gid <- 1 to groups) yield {
+        context.actorOf(Props[GroupActor], EHBNodeConfig.ProcessNamePrefix+gid)
+      }).toList
+      for (g <- gs) {
+        g ! GroupInit(master, loops)
+      }
+    case GroupReady(g) =>   
+      readyCounter.decrement
+      if(readyCounter.isZero){
+        timer.start
+       for (g<-gs) {
+         g ! GroupGO
+       }
+      }
+    case GroupDone(g) =>
+      doneCounter.decrement
+//      println("FINISH " + doneCounter.isZero) 
+      if(doneCounter.isZero){
+        timer.finish
+        timer.report
+        sys.exit()
+      }
+  }
+}
+
+class GroupActor extends Actor {
+  val gMaster = self
+  val receiverDoneCounter = new BenchCounter
+  receiverDoneCounter.set(EHBConstant.GSIZE)
+  val rs:List[ActorRef] = (for (rid <- 1 to EHBConstant.GSIZE) yield {
+    context.actorOf(Props().withCreator(new Receiver(gMaster, EHBConstant.GSIZE)), "receiver_"+rid)
+  }).toList
+  var ss:List[ActorRef] = _
+  var master:ActorRef = _
+  def receive = {
+    case GroupInit(master, loops) => {
+      this.ss = (for (sid <- 1 to EHBConstant.GSIZE) yield {
+        context.actorOf(Props().withCreator(new Sender(rs, loops)), "sender_"+sid)
+      }).toList
+      this.master = master
+      master ! GroupReady(self)      
+    }
+    case GroupGO =>
+      for (s<-ss) {
+        s ! SenderGo(gMaster)
+      }
+    case ReceiverDone(r) =>
+      receiverDoneCounter.decrement
+      if(receiverDoneCounter.isZero){
+        master ! GroupDone(self)
+      }
+  }
+}
+
+case class SenderGo(master:ActorRef) extends SenderMsg
+
+class Sender(rs:List[ActorRef], loops:Int) extends Actor {
+  def receive = {
+    case SenderGo(master) =>
+      sender(rs, loops)
+  }
+  
+  def sender(rs:List[ActorRef], loop:Int):Unit = {
+    if (loop == 0){
+      for (r<-rs){
+        r ! SenderDone
+      }
+    }else if (loop > EHBConstant.ACK){
+      sender_ack(rs, EHBConstant.ACK)
+      sender(rs, loop - EHBConstant.ACK)
+    }else{
+      for (r<-rs){
+        r ! EHBConstant.DATA
+        sender(rs, loop-1)
+      }
+    }
+  }
+
+  def sender_ack(rs:List[ActorRef], n:Int):Unit = {
+    if(n == 2){
+      val ack = context.actorOf(Props[SenderAck2])
+      for (r<-rs){
+        r ! EHBConstant.DATA
+        r ! RUKeepingUp(ack)
+      }      
+    }else{
+      for (r <- rs){
+        r ! EHBConstant.DATA
+      }
+      sender_ack(rs, n-1)
+    }
+  }
+}
+
+class SenderAck2 extends Actor{
+    def receive = {
+      case IamKeeypingUp(receiver) =>
+        receiver ! EHBConstant.DATA
+    }
+}
+
+case class Receiver(val gMaster:ActorRef, var senderLeft:Int) extends Actor{
+  val senderCounter = new BenchCounter
+  senderCounter.set(senderLeft)
+  def receive = {
+    case SenderDone => 
+      senderCounter.decrement
+      if (senderCounter.isZero)
+        gMaster ! ReceiverDone(self)        
+    case RUKeepingUp(from) =>
+      from ! IamKeeypingUp(self)
+    case _ => // do nothing
+  }
+}
+
+
+object EHBBench extends App{
+  private val nodes:Int = args(0).toInt
+  private val groups = 3
+  
+  private val system = ActorSystem("EHBSystem", masterNodeConfig(EHBNodeConfig.WorkerNodePrefix, EHBNodeConfig.ProcessPathPrefix, EHBNodeConfig.ProcessNamePrefix, groups, nodes))  
+  val master = system.actorOf(Props[MasterActor], "EHBBenchActor")
+  master ! MasterGO(groups,3)
+}
+
+object EHBNode extends App {
+  private val nodeID:Int = args(0).toInt
+
+  private val system = ActorSystem(EHBNodeConfig.WorkerNodePrefix+nodeID, WorkerNodeConfig(nodeID))
+}
+
+object EHBNodeConfig {
+  val WorkerNodePrefix = "EHBNode"
+  val ProcessPathPrefix = "EHBBenchActor"
+  val ProcessNamePrefix = "EHBProcess"
+}
---
+++ scalabilityBeowulf/src/main/scala/scalabilityBeowulf/takka/genstress/GenStress.scala
@@ -0,0 +1,125 @@
+package scalabilityBeowulf.takka.genstress
+
+/*
+This is a generic server benchmark that spawns an echo server 
+and a number of clients. Each client fills its message queue 
+with a number of dummy messages; it then sends some messages 
+to the echo server and waits for its response. 
+
+In The Erlang implementation, the benchmark can be executed 
+with or without using the gen_server behaviour.  Because
+gen_server is not required in Akka and TAkka, we port the version
+without using the gen_server behaviour. 
+
+The benchmark is parametrised by the number of clients, dummy messages 
+and messages exchanged with the echo server.
+ */
+import takka.actor.{Actor, ActorRef, ActorSystem, Props}
+import util.{BenchTimer, BenchCounter}
+import akka.remote._
+import com.typesafe.config.ConfigFactory
+import scalabilityBeowulf.BeowulfConfig._
+
+sealed trait TestorMsg
+ /*
+    Arguments: np ( the number of clients ),
+               n ( the number of messages to the server ), 
+               cqueue ( the number of dummy messages ) 
+   */
+case class GenStressTestMsg(np:Int, n:Int, cqueue:Int) extends TestorMsg
+case class ClientEcho(client:ActorRef[ClientMsg]) extends TestorMsg
+
+case class ClientMsg(server:ActorRef[ServerMsg], n:Int, testor:ActorRef[TestorMsg]) extends ClientMessage// from testor
+case class Echo(server:ActorRef[ServerMsg], msg:String) extends ServerMessage with ClientMessage//from server
+
+case class ServerMsg(client:ActorRef[ClientMessage], msg:String) extends ServerMessage//from client
+
+sealed trait ServerMessage
+sealed trait ClientMessage
+
+class GenStressServerActor extends Actor[ServerMsg] {
+  def typedReceive = {
+    case ServerMsg(client, msg) =>
+      client ! Echo(typedSelf, msg)
+  }
+}
+
+class GenStressClientActor extends Actor[ClientMessage] {
+  var n:Int = 0
+  var testor:ActorRef[TestorMsg] = _
+  def typedReceive = {
+    case ClientMsg(server, n, testor) =>
+      this.n = n
+      this.testor = testor
+
+      for (i<-1 to n) {
+        server ! ServerMsg(typedSelf, "dummy message")
+      }
+    case Echo(server, msg) =>
+      this.n -= 1
+      if (n == 0){
+        testor ! ClientEcho(typedSelf)
+      }
+  }
+}
+
+class GenStressTestActor extends Actor[TestorMsg] {
+  var np:Int = 0
+  val timer = new BenchTimer
+  var server:ActorRef[ServerMsg] = _
+  var clients:List[ActorRef[ClientMessage]] = _
+  def typedReceive = {
+    case GenStressTestMsg(np, n, cqueue) =>
+      this.np = np
+     
+      this.server = typedContext.actorOf(Props[ServerMsg, GenStressServerActor])
+      this.clients = (for (i<- 1 to np) yield {
+        typedContext.actorOf(Props[ClientMessage, GenStressClientActor], GenNodeConfig.ProcessNamePrefix+i)        
+      }).toList
+    
+      timer.start
+      
+      for (client <- clients) {
+        client ! ClientMsg(server, n, typedSelf)
+      }
+      
+    case ClientEcho(_) =>
+      np = np-1
+      if (np == 0) {
+        timer.finish
+        timer.report
+        
+        typedContext.stop(server)
+        for (client <- clients) {
+          typedContext.stop(client)
+        }
+        sys.exit
+      }
+    
+  }
+}
+
+
+object EHBBench extends App{
+  private val nodes:Int = args(0).toInt
+  private val processes = 1000
+  
+  private val system = ActorSystem("EHBSystem", masterNodeConfig(GenNodeConfig.WorkerNodePrefix, GenNodeConfig.ProcessPathPrefix, GenNodeConfig.ProcessNamePrefix, processes, nodes))  
+  val master = system.actorOf(Props[TestorMsg, GenStressTestActor], "GenStressBenchActor")
+  master ! GenStressTestMsg(processes, GenNodeConfig.queue, GenNodeConfig.cqueue)
+}
+
+object EHBNode extends App {
+  private val nodeID:Int = args(0).toInt
+
+  private val system = ActorSystem(GenNodeConfig.WorkerNodePrefix+nodeID, WorkerNodeConfig(nodeID))
+}
+
+object GenNodeConfig {
+  val WorkerNodePrefix = "GenStressNode"
+  val ProcessPathPrefix = "GenStressBenchActor"
+  val ProcessNamePrefix = "GenStressProcess"
+    
+  val queue:Int = 3000
+  val cqueue:Int = 5000
+}
