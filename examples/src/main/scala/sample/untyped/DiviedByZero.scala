90224257712f5e6efc345aa2f4df35765f2702c5
Jiansen <jiansenhe2gmail.com>
2013-02-14 23:06:00 +0000

    add gcd to DiviedByZero

--- examples/src/main/scala/sample/untyped/DiviedByZero.scala
+++ examples/src/main/scala/sample/untyped/DiviedByZero.scala
@@ -4,27 +4,36 @@ import akka.actor._
 import scala.concurrent.duration._
 import akka.actor.SupervisorStrategy._
 
-case class Addition(m:Int, n:Int)
-case class Subtraction(m:Int, n:Int)
-case class Multiplication(m:Int, n:Int)
 case class Division(m:Int, n:Int)
+case class GCD(m:Int, n:Int)
 
 class Calculator extends Actor {
   def receive = {
-    case Addition(m, n) => 
-      println(m +" + "+ n +" = "+ (m+n))
-    case Subtraction(m:Int, n:Int) =>
-      println(m +" - "+ n +" = "+ (m-n))
-    case Multiplication(m:Int, n:Int) =>
-      println(m +" * "+ n +" = "+ (m*n))
-    case Division(m:Int, n:Int) =>
+    case Division(m, n) =>
       println(m +" / "+ n +" = "+ (m/n))
+    case GCD(m, n) =>
+     println("GCD of "+m+" and "+n+" is "+gcd(m,n)) 
   }
+  
+  def gcd(m:Int, n:Int):Int = {
+    var tempm = m;
+    var tempn = n
+    while (tempm != tempn){
+      println(tempm + "  "+ tempn)
+      if (tempm > tempn){
+        tempm = tempm-tempn
+      }else{
+        tempn = tempn-tempm
+      }
+    }
+    tempm
+  }
+  
 }
 
 class SafeCalculator extends Actor {
   override val supervisorStrategy =
-    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
+    OneForOneStrategy(maxNrOfRetries = 2, withinTimeRange = 1 minute) {
       case _: ArithmeticException  =>
         println("ArithmeticException Raised to: "+self)
         Restart
@@ -37,18 +46,41 @@ class SafeCalculator extends Actor {
   }
 }
 
+class Super extends Actor{
+  override val supervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 2, withinTimeRange = 1 minute) {
+      case e  =>
+        println("Exception "+e+"Raised to: "+self)
+        Restart
+    }
+
+  val child:ActorRef = context.actorOf(Props[SafeCalculator], "safe")
+
+  def receive = {
+    case m => child ! m
+  }  
+}
+
 object SupervisorTest extends App{
   val system = ActorSystem("MySystem")
-  val actorRef:ActorRef = system.actorOf(Props[SafeCalculator], "safecalculator")
+//  val calculator:ActorRef = system.actorOf(Props[SafeCalculator], "safecalculator")
+  val calculator:ActorRef = system.actorOf(Props[Super], "super")
 
-  actorRef ! Addition(3, 2)
-  actorRef ! Division(10, 0)
-  actorRef ! Division(10, 5)
+  calculator ! Division(10, 0)
+  calculator ! Division(10, 5)
+  calculator ! Division(10, 0)
+  calculator ! Division(10, 5)
+  calculator ! Division(10, 0)
+  calculator ! Division(10, 5)
+//  actorRef ! GCD(0, 72)
+  calculator ! Division(12, 6)
 }
 
 /*
 Terminal Output:
-3 + 2 = 5
 ArithmeticException Raised to: Actor[akka://MySystem/user/safecalculator]
 10 / 5 = 2
+ArithmeticException Raised to: Actor[akka://MySystem/user/safecalculator]
+10 / 5 = 2
+ArithmeticException Raised to: Actor[akka://MySystem/user/safecalculator]
 */
