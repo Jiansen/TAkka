72e80be5db9473f6c5bb7342eccdaddab2099704
Jiansen <jiansenhe@gmail.com>
2013-10-04 00:02:41 +0100

    add scala example

---
+++ examples/src/main/scala/sample/scala/ParameterTypeDemo3.scala
@@ -0,0 +1,25 @@
+package sample.scala
+
+class Super
+class Middle extends Super
+class Sub extends Middle
+
+class Base3[A>:Sub <:Super, B]{
+  def p(a:A, b:B){
+    println("a: "+a+"\nb: "+b)
+  }
+}
+
+// class Foo3[B] extends Base3[Int, B]
+// type arguments [Int,B] do not conform to class Base3's type parameter bounds [A >: sample.scala.Sub <: sample.scala.Super,B]
+
+class Foo3[B] extends Base3[Sub, B]
+class Bar3 extends Foo3[Int]
+
+object ParameterTypeDemo3 extends App{
+  val bar = new Bar3()
+  bar.p(new Sub, 2)
+//  bar.p(new Middle, 2)
+//   type mismatch; found : sample.scala.Middle required: 
+//	 sample.scala.Sub
+}
