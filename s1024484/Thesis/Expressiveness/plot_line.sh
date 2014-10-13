9abb8ee64a25648eb0a27d1ce7df6973b69477d3
jiansen <jiansen@gmail.com>
2014-10-13 21:09:10 +0100

    final commit

--- s1024484/Thesis/Expressiveness/plot_line.sh
+++ s1024484/Thesis/Expressiveness/plot_line.sh
@@ -15,9 +15,8 @@ set ylabel 'lines of Code (Log10)'
 set xrange [0:5]
 set yrange [0:5]
 
-plot "exp.dat" using 6:7 with points  title "Modified TAkka Lines" lc 1 ,\
-     "exp.dat" using 6:8 with points  title "TAkka Code Lines" lc 2
-
+plot "exp.dat" using 9:11 with points  title "TAkka Code Lines" lc 1 ,\
+     "exp.dat" using 9:10 with points  title "Modified TAkka Lines" lc 2
 
 set output
 
--- s1024484/Thesis/Expressiveness/plot_percent.sh
+++ s1024484/Thesis/Expressiveness/plot_percent.sh
@@ -9,14 +9,17 @@ set output  "Expressiveness2.png"
 # set title "Expressiveness Checks 2"
 
 set xlabel 'lines of Akka Code (Log10)'
-set ylabel 'lines of Code'
+set ylabel '% of Code'
 
 
 set xrange [0:5]
 set yrange [0:120]
 
-plot "exp.dat" using 6:2 with points  title "% of Modified Code" lc 1 ,\
-     "exp.dat" using 6:3 with points  title "% of Code Size" lc 2
+plot "exp.dat" using 9:3 with points  title "% of Code Size" lc 2 ,\
+     "exp.dat" using 9:2 with points  title "% of Modified Code" lc 1 ,\
+     "exp.dat" using 9:12 with points  title "% of New Code" lc 3 ,\
+     "exp.dat" using 9:13 with points  title "% of Updated Code" lc 4 ,\
+     "exp.dat" using 9:14 with points  title "% of Deleted Code" lc 5
 
 
 set output
